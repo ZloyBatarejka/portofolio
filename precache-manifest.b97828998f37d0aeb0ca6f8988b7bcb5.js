2adaf69bbcfe61bf4c71b8af8f9efb964c963d68
Yakovlev Ruslanz <57760502+ZloyBatarejka@users.noreply.github.com>
2019-12-18 21:20:45 +0300

    Updates

---
+++ precache-manifest.b97828998f37d0aeb0ca6f8988b7bcb5.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e214577b3600850f54dc5d539cdff9c8",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "6617ae4e9153ad958dfd",
+    "url": "/portofolio/static/css/main.98ed2bbb.chunk.css"
+  },
+  {
+    "revision": "9173e98047f6916be5e5",
+    "url": "/portofolio/static/js/2.de8546f0.chunk.js"
+  },
+  {
+    "revision": "6617ae4e9153ad958dfd",
+    "url": "/portofolio/static/js/main.8faf3ef1.chunk.js"
+  },
+  {
+    "revision": "24a84e09a595f5aefaef",
+    "url": "/portofolio/static/js/runtime-main.d5d004b3.js"
+  },
+  {
+    "revision": "765dc57e18f4cd8e46afa316ed4c26e4",
+    "url": "/portofolio/static/media/again.765dc57e.jpg"
+  },
+  {
+    "revision": "ea773e426b598b7eba4989a9ef3645b5",
+    "url": "/portofolio/static/media/background.ea773e42.jpg"
+  },
+  {
+    "revision": "f75d8d273796cbdae5fda9e65a09dc30",
+    "url": "/portofolio/static/media/bird.f75d8d27.jpg"
+  },
+  {
+    "revision": "078958195e132387604b49a78862685b",
+    "url": "/portofolio/static/media/bootstrain.07895819.jpg"
+  },
+  {
+    "revision": "f8b3744b9509467d43af4fa963402ece",
+    "url": "/portofolio/static/media/boxShadow.f8b3744b.jpg"
+  },
+  {
+    "revision": "dd11a894f8fdfc17443e7b6e623dab5f",
+    "url": "/portofolio/static/media/bt4.dd11a894.jpg"
+  },
+  {
+    "revision": "113c94d3820dad95505a490d9f68eb32",
+    "url": "/portofolio/static/media/calculator.113c94d3.jpg"
+  },
+  {
+    "revision": "c654fb747fc45df8cd08add4f5fab1a3",
+    "url": "/portofolio/static/media/clock.c654fb74.jpg"
+  },
+  {
+    "revision": "92049381829677be15739d02d002f931",
+    "url": "/portofolio/static/media/dots.92049381.jpg"
+  },
+  {
+    "revision": "1a20534ddd93f1ba79bf12e53295d183",
+    "url": "/portofolio/static/media/keycode.1a20534d.jpg"
+  },
+  {
+    "revision": "75f9918ce555d59c432e26ee4c29eee2",
+    "url": "/portofolio/static/media/lightbox.75f9918c.jpg"
+  },
+  {
+    "revision": "028244cfb40945dc41de6efb676c99fd",
+    "url": "/portofolio/static/media/maddyson.028244cf.jpg"
+  },
+  {
+    "revision": "cbd1e7415c290167d5ac6b69f8d8e895",
+    "url": "/portofolio/static/media/menu.cbd1e741.jpg"
+  },
+  {
+    "revision": "0dd9713c5faf52110ae3601c8292bd2c",
+    "url": "/portofolio/static/media/mouseover.0dd9713c.jpg"
+  },
+  {
+    "revision": "d78b3ad00b97e1a5935189c6bd9bed83",
+    "url": "/portofolio/static/media/music.d78b3ad0.jpg"
+  },
+  {
+    "revision": "91793208c89040fda44000d92f701e9c",
+    "url": "/portofolio/static/media/no.91793208.jpg"
+  },
+  {
+    "revision": "5d8e663363b31f87e4313c1f24d2a232",
+    "url": "/portofolio/static/media/password.5d8e6633.jpg"
+  },
+  {
+    "revision": "505ff2bcd27cb5857191854b7c9b9f18",
+    "url": "/portofolio/static/media/quiz.505ff2bc.jpg"
+  },
+  {
+    "revision": "54777e72599e58aef7b33ccd75bebfdd",
+    "url": "/portofolio/static/media/red.54777e72.jpg"
+  },
+  {
+    "revision": "ee047cf3b6e4b0abb74daf549dd2dd43",
+    "url": "/portofolio/static/media/revolution.ee047cf3.jpg"
+  },
+  {
+    "revision": "02e8766f9806c148d4e162cf96bc66e1",
+    "url": "/portofolio/static/media/snake.02e8766f.jpg"
+  },
+  {
+    "revision": "493e7d983a013b80d53d73482bdbe424",
+    "url": "/portofolio/static/media/ticker.493e7d98.jpg"
+  },
+  {
+    "revision": "8b34e6444edefc52023e6278428ff12a",
+    "url": "/portofolio/static/media/timer.8b34e644.jpg"
+  },
+  {
+    "revision": "82a3259d6cd99b8a1876944eb2e8eccc",
+    "url": "/portofolio/static/media/todo.82a3259d.jpg"
+  },
+  {
+    "revision": "9ed6de0e13197811b394bc371353904c",
+    "url": "/portofolio/static/media/witcher.9ed6de0e.jpg"
+  }
+]);
